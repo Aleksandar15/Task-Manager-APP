ba38a7aaf53b37617c8aa006e268423489350bb0
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-01-05 17:26:02 +0100

    Adding FRONTEND

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // jshint esversion: 8
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
-  console.log('Server started on port ' + process.env.PORT);
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`)
+})
